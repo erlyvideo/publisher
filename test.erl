ea9e600de4a1c30940aa07c612c539a63e991707
Max Lapshin <max@maxidoors.ru>
2011-08-21 18:57:39 +0400

    encoder preset

--- test.erl
+++ test.erl
@@ -1,5 +1,5 @@
 #!/usr/bin/env escript
-%%! +A 16 +a 4096
+%%! +A 4 +a 4096
 
 % -include_lib("erlmedia/include/video_frame.hrl").
 
@@ -10,7 +10,7 @@ main([]) ->
   erlang:system_flag(scheduler_bind_type, spread),
   io:format("~p~n", [erlang:system_info(scheduler_bind_type)]),
   io:format("~p~n", [erlang:system_info(multi_scheduling)]),
-  io:format("~p,~p,~p~n", [erlang:system_info(smp_support),erlang:system_info(threads),erlang:system_info(thread_pool_size)]),
+  io:format("~p~n", [{erlang:system_info(smp_support),erlang:system_info(threads),erlang:system_info(thread_pool_size),erlang:system_info(schedulers_online)}]),
   code:add_pathz("apps/publisher/ebin"),
   publisher:run(),
   receive
