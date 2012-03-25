81ec762cbe5002b3a380c55ef9930298ca06cb06
Max Lapshin <max@maxidoors.ru>
2012-03-25 22:05:06 +0400

    fixed logging

--- apps/rtmp/src/log.hrl
+++ apps/rtmp/src/log.hrl
@@ -1,2 +1,1 @@
--define(D(X), (case application:get_env(rtmp, logging_function) of undefined -> io:format("~p:~p ~240p~n", [?MODULE, ?LINE, X]); _ -> (element(2,application:get_env(rtmp,logging_function)))(?MODULE, ?LINE, X) end)).
-
+-define(D(X), io:format("~p:~p ~240p~n", [?MODULE, ?LINE, X])).
